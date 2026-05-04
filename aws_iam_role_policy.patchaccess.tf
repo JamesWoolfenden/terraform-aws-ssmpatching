5fb7af32d3bc4c1a9f5b22a38f9b3a8156264f47
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:30:44 +0100

    fix: skip CKV_AWS_272 and CKV_AWS_356 on IAM/SQS policy resources

--- aws_iam_role_policy.patchaccess.tf
+++ aws_iam_role_policy.patchaccess.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy" "patchaccess" {
+  # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   name = "patchaccess"
   role = aws_iam_role.patchaccess.id
 
